22aa3edc30c931a5505b688e06341b80f5611ee0
youlei202 <lei.you@pm.me>
2023-10-20 20:37:07 +0200

    added wasserstein distance

--- utils/distances.py
+++ utils/distances.py
@@ -1,38 +1,40 @@
 import ot
+import numpy as np
+from typing import Optional
 
 
-def sliced_wasserstein_distance(X_s, X_t, thetas, a=None, b=None, metric="sqeuclidean"):
-    """
-    Compute the sliced Wasserstein distance between X_s and X_t
+class SlicedWassersteinDivergence:
+    def __init__(self, dim: int, n_proj: int):
+        self.dim = dim
+        self.n_proj = n_proj
+        self.thetas = np.random.randn(n_proj, dim)
+        thetas /= np.linalg.norm(thetas, axis=1)[:, None]
 
-    Parameters:
-    X_s : np.ndarray, shape (n_samples_a, dim)
-        samples in the source domain
-    X_t : np.ndarray, shape (n_samples_b, dim)
-        samples in the target domain
-    a : np.ndarray, shape (n_samples_a,), optional
-        weights of each sample of X_s, default is uniform weight
-    b : np.ndarray, shape (n_samples_b,), optional
-        weights of each sample of X_t, default is uniform weight
-    metric : str, optional
-        metric to be used for Wasserstein-1 distance computation
-    n_projections : int, optional
-        number of projections
+    def distance(self, X_s: np.array, X_t: np.array, p: Optional[int] = 2):
+        """
+        Compute the sliced Wasserstein distance between X_s and X_t
 
-    Returns:
-    swd : float
-        Sliced Wasserstein Distance between X_s and X_t
-    """
-    # Generate random projection vectors on the unit sphere
+        Parameters:
+        X_s : np.ndarray, shape (n_samples_a, dim)
+            samples in the source domain
+        X_t : np.ndarray, shape (n_samples_b, dim)
+            samples in the target domain
+        metric : str, optional
+            metric to be used for Wasserstein-1 distance computation
 
-    swd = 0
-    n_projections = len(thetas)
-    for theta in thetas:
-        # Project data onto the vector theta
-        proj_X_s = X_s.dot(theta)
-        proj_X_t = X_t.dot(theta)
+        Returns:
+        swd : float
+            Sliced Wasserstein Distance between X_s and X_t
+        """
+        # Generate random projection vectors on the unit sphere
 
-        # Compute 1D Wasserstein distance and accumulate
-        swd += ot.wasserstein_1d(proj_X_s, proj_X_t, a, b, p=2)
+        swd = 0
+        for theta in self.thetas:
+            # Project data onto the vector theta
+            proj_X_s = X_s.dot(theta)
+            proj_X_t = X_t.dot(theta)
 
-    return swd / n_projections
+            # Compute 1D Wasserstein distance and accumulate
+            swd += ot.wasserstein_1d(proj_X_s, proj_X_t, None, None, p=p)
+
+        return swd / self.n_proj

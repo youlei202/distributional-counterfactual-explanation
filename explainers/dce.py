3d3134e26de1dc22205d7c3521669a017d69752a
youlei202 <lei.you@pm.me>
2023-10-22 14:45:03 +0200

    runnable

--- explainers/dce.py
+++ explainers/dce.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 import torch
+from explainers.distances import SlicedWassersteinDivergence, WassersteinDivergence
+from typing import Optional
+import numpy as np
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class DistributionalCounterfactualExplainer:
-    def __init__(self, model):
+    def __init__(self, model, X, epsilon=0.1, lambda_val=0.5, n_proj=50):
         """
         Initialize the counterfactual explainer.
 
@@ -12,7 +17,30 @@ class DistributionalCounterfactualExplainer:
         """
         self.model = model
 
-    def _compute_gradient(self, K, X, X_prime, mu, nu, lambda_val):
+        self.X = torch.from_numpy(X).float()
+        self.X_prime = self.X.clone()
+
+        self.X.requires_grad_(True).retain_grad()
+        self.best_X = None
+
+        self.y = self.model(self.X)
+        self.y_prime = self.y.clone()
+
+        self.best_y = None
+
+        self.swd = SlicedWassersteinDivergence(X.shape[1], n_proj=n_proj)
+        self.wd = WassersteinDivergence()
+
+        # Introduce an optimizer for self.X
+
+        self.Q = torch.tensor(torch.inf, dtype=torch.float)
+        self.best_Q = self.Q
+
+        self.epsilon = epsilon
+
+        self.lambda_val = lambda_val
+
+    def _compute_gradient_for_X(self, mu_list, nu):
         """
         Compute the gradient of Q(X) with respect to each x_i in X.
 
@@ -29,35 +57,82 @@ class DistributionalCounterfactualExplainer:
         - Gradient tensor of shape (n, d).
         """
 
-        X = torch.from_numpy(X).float()
-        X_prime = torch.from_numpy(X_prime).float()
+        n, m = self.X.shape[0], self.X_prime.shape[0]
 
-        # lambda_val = torch.Tensor(lambda_val).double()
-
-        # Ensure X requires gradient for autograd
-        X.requires_grad_(True)
-
-        # Initialize Q value to 0
-        Q = torch.tensor(0.0, dtype=torch.float)
-
-        n, m = X.shape[0], X_prime.shape[0]
-
-        for k, theta in enumerate(K):
+        # Compute the first term
+        self.term1 = torch.tensor(0.0, dtype=torch.float)
+        for k, theta in enumerate(self.swd.thetas):
+            mu = mu_list[k]
             theta = torch.from_numpy(theta).float()
             for i in range(n):
                 for j in range(m):
-                    term1 = (
-                        mu[k][i, j]
-                        * (torch.dot(theta, X[i]) - torch.dot(theta, X_prime[j])) ** 2
+                    self.term1 += (
+                        mu[i, j]
+                        * (
+                            torch.dot(theta, self.X[i])
+                            - torch.dot(theta, self.X_prime[j])
+                        )
+                        ** 2
                     )
-                    term2 = (
-                        lambda_val
-                        * nu[i, j]
-                        * (self.model(X[i]) - self.model(X_prime[j])) ** 2
-                    )
-                    Q += term1 + term2.item()
+        self.term1 /= self.swd.n_proj
+
+        # Compute the second term
+        term = torch.tensor(0.0, dtype=torch.float)
+        for i in range(n):
+            for j in range(m):
+                term += (
+                    nu[i, j]
+                    * (self.model(self.X[i]) - self.model(self.X_prime[j])) ** 2
+                ).item()
+        self.term2 = self.lambda_val * (self.epsilon - term)
+
+        # Compute the objective function Q
+        self.Q = self.term1 + self.term2
 
         # Compute gradient
-        Q.backward()
+        self.Q.backward()
 
-        return X.grad
+        return self.X.grad
+
+    def optimize(
+        self,
+        initial_lr: Optional[float] = 0.1,
+        max_iter: Optional[int] = 100,
+        tol=1e-6,
+        weight_decay=0.0,
+    ):
+
+        optimizer = optim.Adam([self.X], lr=initial_lr, weight_decay=weight_decay)
+        scheduler = ReduceLROnPlateau(
+            optimizer, "min", patience=10, factor=0.5, verbose=True
+        )
+
+        past_Qs = [float("inf")] * 5  # Store the last 5 Q values for moving average
+        for i in range(max_iter):
+            optimizer.zero_grad()
+            self.swd.distance(X_s=self.X, X_t=self.X_prime)
+            self.wd.distance(y_s=self.y, y_t=self.y_prime)
+
+            self._compute_gradient_for_X(mu_list=self.swd.mu_list, nu=self.wd.nu)
+            optimizer.step()
+
+            # scheduler.step(self.Q)
+
+            self.y = self.model(self.X)
+
+            if self.Q < self.best_Q:
+                self.best_Q = self.Q.clone().detach()
+                self.best_X = self.X.clone().detach()
+                self.best_y = self.y.clone().detach()
+
+            # Check for convergence using moving average of past Q changes
+            past_Qs.pop(0)
+            past_Qs.append(self.Q.item())
+            avg_Q_change = (past_Qs[-1] - past_Qs[0]) / 5
+            if abs(avg_Q_change) < tol:
+                print(f"Converged at iteration {i+1}")
+                break
+
+            print(
+                f"Iter {i+1}: Q = {self.Q}, term1 = {self.term1}, term2 = {self.term2}"
+            )
---
+++ explainers/distances.py
@@ -0,0 +1,79 @@
+import ot
+import numpy as np
+from typing import Optional
+import torch
+
+
+class WassersteinDivergence:
+    def __init__(self):
+        self.nu = None
+
+    def distance(self, y_s: np.array, y_t: np.array):
+
+        proj_y_s_dist_mass = torch.ones(len(y_s)) / len(y_s)
+        proj_y_t_dist_mass = torch.ones(len(y_t)) / len(y_t)
+
+        M_y = ot.dist(
+            y_s.reshape(y_s.shape[0], 1),
+            y_t.reshape(y_t.shape[0], 1),
+            metric="sqeuclidean",
+        )
+
+        self.nu = ot.emd(proj_y_s_dist_mass, proj_y_t_dist_mass, M_y)
+        dist = torch.sum(self.nu * M_y)
+
+        return dist
+
+
+class SlicedWassersteinDivergence:
+    def __init__(self, dim: int, n_proj: int):
+        self.dim = dim
+        self.n_proj = n_proj
+        self.thetas = np.random.randn(n_proj, dim)
+        self.thetas /= np.linalg.norm(self.thetas, axis=1)[:, None]
+
+        self.mu_list = []
+        self.nu = None
+
+    def distance(self, X_s: np.array, X_t: np.array):
+        """
+        Compute the sliced Wasserstein distance between X_s and X_t
+
+        Parameters:
+        X_s : np.ndarray, shape (n_samples_a, dim)
+            samples in the source domain
+        X_t : np.ndarray, shape (n_samples_b, dim)
+            samples in the target domain
+        metric : str, optional
+            metric to be used for Wasserstein-1 distance computation
+
+        Returns:
+        swd : float
+            Sliced Wasserstein Distance between X_s and X_t
+        """
+
+        proj_X_s_dist_mass = torch.ones(len(X_s)) / len(X_s)
+        proj_X_t_dist_mass = torch.ones(len(X_t)) / len(X_t)
+
+        self.mu_list = []
+        dist = 0
+        for theta in self.thetas:
+            
+            # Project data onto the vector theta
+            theta = torch.from_numpy(theta).float()
+            proj_X_s = X_s @ theta
+            proj_X_t = X_t @ theta
+
+            M_x = ot.dist(
+                proj_X_s.reshape(proj_X_s.shape[0], 1),
+                proj_X_t.reshape(proj_X_t.shape[0], 1),
+                metric="sqeuclidean",
+            )
+
+            # Compute 1D Wasserstein distance and accumulate
+            mu = ot.emd(proj_X_s_dist_mass, proj_X_t_dist_mass, M_x)
+            self.mu_list.append(mu)
+
+            dist += torch.sum(mu * M_x)
+
+        return dist / self.n_proj
